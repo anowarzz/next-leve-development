e861f8265156eb83b31d06ec6ece025f4a914bd3
Anowar Hosen <anowarhosen444@gmail.com>
2025-05-01 23:55:36 -0400

    use of types in function

---
+++ basic-types-of-typescript/dist/object-optional-literal.js
@@ -0,0 +1,9 @@
+"use strict";
+//  ==> Reference Data type === Object
+const user = {
+    firstName: "Chandler",
+    middleName: "Matthew",
+    lastName: "Bing",
+    isMarried: true,
+    company: "Nobody Really Knows",
+};
