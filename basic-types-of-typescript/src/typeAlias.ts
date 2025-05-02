1928e7bb322ca8a0b748101544ea8f2d268d6cf6
Anowar Hosen <anowarhosen444@gmail.com>
2025-05-02 12:47:54 -0400

    type alias practiced

---
+++ basic-types-of-typescript/src/typeAlias.ts
@@ -0,0 +1,46 @@
+{
+  type Student = {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo?: string;
+    address: string;
+  };
+
+  const student1: Student = {
+    name: "Ross Geller",
+    age: 30,
+    gender: "Male",
+    contactNo: "123-456-7890",
+    address: "123 Main St, Springfield",
+  };
+
+  const student2: Student = {
+    name: "Rachel Green",
+    age: 28,
+    gender: "Female",
+    contactNo: "987-654-3210",
+    address: "456 Elm St, Springfield",
+  };
+
+  const student3: Student = {
+    name: "Monica Geller",
+    age: 29,
+    gender: "Female",
+    contactNo: "555-123-4567",
+    address: "789 Oak St, Springfield",
+  };
+
+  //  type alias with number
+  type UserName = string;
+  const userName: UserName = "Anowar";
+  type IsAdmin = boolean;
+  const isAdmin: IsAdmin = true;
+
+  //  Type alias for function
+  type Add = (num1: number, num2: number) => number;
+
+  const add: Add = (num1, num2) => num1 + num2;
+
+  
+}
