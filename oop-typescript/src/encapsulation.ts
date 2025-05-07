b4eb753b5473fe3c9f2cd195557ecdbb8cfe73f6
Anowar Hosen <anowarhosen444@gmail.com>
2025-05-07 10:57:30 +0600

    learned about encapsulation

---
+++ oop-typescript/src/encapsulation.ts
@@ -0,0 +1,4 @@
+// What is encapsulation  a interview ready solid short answer
+/*/ 
+*  Encapsulation is an OOP principle that binds data and methods into a single unit and restricts direct access to some parts of the object. It helps protect the internal state of an object and promotes data hiding, ensuring better control and security.
+ /*/
