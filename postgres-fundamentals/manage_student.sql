738a0d4fe42616b2ecb26c7ff5147c48b644b1f7
Anowar Hosen <anowarhosen444@gmail.com>
2025-05-19 21:29:31 +0600

    update and delete problems practiced

--- postgres-fundamentals/manage_student.sql
+++ postgres-fundamentals/manage_student.sql
@@ -30,16 +30,16 @@ INSERT INTO
         (1005, 'Naim Islam', 21, 'EEE', 59, 'passed', '2025-05-14'),
         (1006, 'Mehzabin Alam', 22, 'ENG', 80, 'passed', '2025-05-13'),
         (1007, 'Shahriar Nafi', 20, 'CSE', 90, 'passed', '2025-05-12'),
-        (1008, 'Tamanna Zaman', 22, 'CSE', 30, 'failed', '2025-05-11'),
+        (1008, 'Tamanna Zaman', 22, 'CSE', 30, 'failed', '2022-05-11'),
         (1009, 'Arefin Rafi', 24, 'BBA', 68, 'passed', '2025-05-10'),
         (1010, 'Jannat Jahan', 21, 'EEE', 85, 'passed', '2025-05-09'),
         (1011, 'Rakib Chowdhury', 23, 'CSE', 39, 'failed', '2025-05-08'),
         (1012, 'Maliha Noor', 22, 'ENG', 75, 'passed', '2025-05-07'),
-        (1013, 'Zarif Hossain', 21, 'CSE', 94, 'passed', '2025-05-06'),
+        (1013, 'Zarif Hossain', 21, 'CSE', 94, 'passed', '2021-05-06'),
         (1014, 'Fahmida Kabir', 20, 'BBA', 62, 'passed', '2025-05-05'),
         (1015, 'Rony Ahmed', 23, 'EEE', 40, 'failed', '2025-05-04'),
         (1016, 'Farzana Sultana', 21, 'ENG', 82, 'passed', '2025-05-03'),
-        (1017, 'Arif Mahmud', 22, 'CSE', 47, 'failed', '2025-05-02'),
+        (1017, 'Arif Mahmud', 22, 'CSE', 47, 'failed', '2020-05-02'),
         (1018, 'Sultana Jahan', 24, 'EEE', 76, 'passed', '2025-05-01'),
         (1019, 'Kawsar Rahman', 22, 'BBA', 50, 'passed', '2025-04-30'),
         (1020, 'Nusrat Anjum', 21, 'ENG', 89, 'passed', '2025-04-29');
@@ -119,3 +119,22 @@ SELECT max(age) as "Maximum Age"from students
 
 -- Get the minimum age
 SELECT min(age) as "Maximum Age"from students
+
+
+--------> Update & Delete Operations <---------
+-- Update the status of students who scored less than 70 to 'failed'.
+UPDATE students 
+    set status = 'failed'
+    WHERE score < 70
+
+    SELECT * from students ;
+
+
+-- Delete students who have not logged in since last year.
+DELETE FROM students
+WHERE last_login < CURRENT_DATE - INTERVAL '1 year';
+
+
+-- Use LIMIT and OFFSET to fetch the second page of results (5 per page).
+
+SELECT * FROM students ORDER BY id LIMIT 5 OFFSET 5 * 1
