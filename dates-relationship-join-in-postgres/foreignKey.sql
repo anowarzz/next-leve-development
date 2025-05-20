544d3f26d92ff676aa97710a5ed5b2dfb5cbf292
Anowar Hosen <anowarhosen444@gmail.com>
2025-05-20 08:35:25 +0600

    learned about referencial integrity while insertion

--- dates-relationship-join-in-postgres/foreignKey.sql
+++ dates-relationship-join-in-postgres/foreignKey.sql
@@ -12,3 +12,37 @@ CREATE TABLE post (
     title text NOT NULL,
     user_id INTEGER REFERENCES "user" (id)
 )
+
+
+-- addint user id not null constraint
+ALTER TABLE post
+ AlTER COLUMN user_id set NOT NULL ;
+
+
+--  inserting data into user table
+INSERT INTO "user" (username) VALUES ('akash'), ('batash'), ('sagor'), ('nodi');
+
+SELECT * from "user"
+
+
+-- inserting data into post table
+INSERT INTO post (title, user_id) VALUES ('Enjoying a sunny day with Akash! ☀️', 2),
+('Batash just shared an amazing recipe! 🍲', 1),
+('Exploring adventures with Sagor.🌟', 4),
+('Nodi''s wisdom always leaves me inspired. 📚', 4);
+
+SELECT * from post
+
+
+-- Insertion constraint on INSERT post
+-- Attempting to insert a post with a user ID that does not exist
+-- Inserting a post with a valid user ID
+-- Attempting to insert a post without specifying a user ID
+
+
+
+
+
+
+-- can't insert a null user id
+INSERT INTO post (title, user_id) VALUES ('test', NULL)
