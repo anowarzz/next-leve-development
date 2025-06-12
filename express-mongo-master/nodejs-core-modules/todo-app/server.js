9251a3aed8f92a5be30e7f44b8503988b6a56ae7
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-12 11:45:30 +0600

    created todo server routes with node js

--- express-mongo-master/nodejs-core-modules/todo-app/server.js
+++ express-mongo-master/nodejs-core-modules/todo-app/server.js
@@ -1,11 +1,27 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
 
-  res.end("Welcome to the Todo App server");
+  if (req.url === "/") {
+    res.end("Welcome to the Todo App server");
+  }
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All todos Here");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Created a new todo");
+  } else {
+    res.end("Route not found");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
   console.log("✔ Todo server listening to port 5000");
 });
+
+
+
+/**
+ *  /todos => All todos
+ *  /todos/create-todos ==> Post/Create a todos
+ */
