0cfc3ed736c5838c7ef79ba4f77459f4b05b07e2
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-13 20:17:17 +0600

    connected the todo app server with mongodb

--- express-mongo-master/foundation-of-express/src/app/server.ts
+++ express-mongo-master/foundation-of-express/src/app/server.ts
@@ -1,10 +1,25 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app";
 
 let server;
 const PORT = process.env.PORT || 5000;
 
+const uri =
+  "mongodb+srv://mongoTodoUser:mongouser@cluster0.udcvnpg.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const bootstrap = async () => {
-  server = app.listen(PORT, () => {
+  await client.connect();
+  console.log("Connected To Mongodb");
+
+  server = app.listen(PORT, async () => {
     console.log(`Example app listening on port ${PORT}`);
   });
 };
