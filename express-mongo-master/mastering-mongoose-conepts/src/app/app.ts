1ae4e761c457cb0b98918c2f7496083b4051c1fc
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-17 09:43:08 +0600

    created first note with mongoose schema and model

--- express-mongo-master/mastering-mongoose-conepts/src/app/app.ts
+++ express-mongo-master/mastering-mongoose-conepts/src/app/app.ts
@@ -1,7 +1,33 @@
 import express, { Application, Request, Response } from "express";
+import { model, Schema } from "mongoose";
 
 const app: Application = express();
 
+const noteSchema = new Schema({
+  title: String,
+  content: String,
+});
+
+const Note = model("Note", noteSchema);
+
+// creating a note
+app.post("/create-note", async (req: Request, res: Response) => {
+  const myNote = new Note({
+    title: "Experss",
+    content: "I want express mastery",
+  });
+
+  const savedNote = await myNote.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Note Created Successfully",
+    data: savedNote,
+  });
+});
+
+
+// server status
 app.get("/", (req: Request, res: Response) => {
   try {
     res.send("Hello From Note App");
