2599f461dfa0827b699a333d7503778f104aee39
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-26 23:18:34 +0600

    intro: created a react app for redux introduction

---
+++ redux-reaper/intro-to-redux/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Counte with Redux</h1>
+
+        <button>Increment</button>
+        <button>0</button>
+        <button>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
