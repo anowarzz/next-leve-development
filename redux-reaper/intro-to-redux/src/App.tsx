8e4b804850bbe7653d04e0431d828526de3db001
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-27 10:23:36 +0600

    feat: add increment and decrement action with type safety

--- redux-reaper/intro-to-redux/src/App.tsx
+++ redux-reaper/intro-to-redux/src/App.tsx
@@ -1,12 +1,27 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+import type { RootState } from "./redux/store";
+
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state: RootState) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <div>
         <h1>Counte with Redux</h1>
 
-        <button>Increment</button>
-        <div>0</div>
-        <button>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <div>{count}</div>
+        <button onClick={handleDecrement}>Decrement</button>
       </div>
     </>
   );
