5b066c1f0ca0edfc8d6bc2a83b5620776cb88646
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-27 10:44:14 +0600

    feat: add increment and decrement by dynamic value

--- redux-reaper/intro-to-redux/src/App.tsx
+++ redux-reaper/intro-to-redux/src/App.tsx
@@ -6,22 +6,23 @@ function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state: RootState) => state.counter);
 
-  const handleIncrement = () => {
-    dispatch(increment());
-  };
+  // const handleIncrement = (amount: number) => {
+  //   dispatch(increment(amount));
+  // };
 
-  const handleDecrement = () => {
-    dispatch(decrement());
-  };
+  // const handleDecrement = (amount: number) => {
+  //   dispatch(decrement(amount));
+  // };
 
   return (
     <>
       <div>
         <h1>Counte with Redux</h1>
 
-        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={() => dispatch(increment(5))}>Increment By 5</button>
+        <button onClick={() => dispatch(increment(1))}>Increment</button>
         <div>{count}</div>
-        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={() => dispatch(decrement(1))}>Decrement</button>
       </div>
     </>
   );
--- redux-reaper/intro-to-redux/src/redux/features/counter/counterSlice.ts
+++ redux-reaper/intro-to-redux/src/redux/features/counter/counterSlice.ts
@@ -1,27 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit"; ;
-
+import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    count: 0
-}
-
+  count: 0,
+};
 
 const counterSlice = createSlice({
-    name: "counter" ,
-    initialState,
-    reducers: {
-        increment : (state) => {
-        state.count = state.count + 1
-        }
-        ,
-        decrement : (state) => {
-        state.count = state.count - 1
-        }
-    }
-  
-})
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.count = state.count + action.payload;
+    },
+    decrement: (state, action) => {
+      state.count = state.count - action.payload;
+    },
+  },
+});
 
+export const { increment, decrement } = counterSlice.actions;
 
-
-export const {increment, decrement} = counterSlice.actions ;
-
-export default counterSlice.reducer ;
+export default counterSlice.reducer;
