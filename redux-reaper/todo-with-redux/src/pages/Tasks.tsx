9b85170e06f7f7f0fe69bc82e38f75ef35ef088f
Anowar Hosen <anowarhosen444@gmail.com>
2025-06-28 11:37:40 +0600

    feat: add task modal form to task input

--- redux-reaper/todo-with-redux/src/pages/Tasks.tsx
+++ redux-reaper/todo-with-redux/src/pages/Tasks.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/module/tasks/AddTaskModal";
 import TaskCard from "@/module/tasks/TaskCard";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -10,8 +11,9 @@ const Tasks = () => {
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div>
-        <h1>Tasks</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-center font-bold text-2xl">Tasks</h1>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
     {
